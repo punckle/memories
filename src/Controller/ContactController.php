946589296c882163430032ae9d4272450e79ef76
Manon Baillet <baillet.manon@gmail.com>
2019-09-03 13:07:45 +0200

    Formulaire contact OK

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,14 +12,30 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request)
+    public function index(Request $request,\Swift_Mailer $mailer)
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            dd($data['mail']);
+            $mail = (new \Swift_Message("Formulaire de contact - 60 ans d'Alain"))
+                ->setFrom($data['mail'])
+                ->setTo('baillet.manon@gmail.com')
+                ->setBody($this->renderView(
+                    'messages/contact.html.twig', [
+                        'subject' => $data['subject'],
+                        'description' => $data['description'],
+                        'contact' => $data['mail']
+                    ]
+                ),
+                    'text/html'
+                );
+            $mailer->send($mail);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé. Quelqu\'un y répondra dès que possible');
+
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('contact/index.html.twig', [
